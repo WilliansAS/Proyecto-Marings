369fe900f312105195ac909ce9c2a0289141e287
WilliansAS <anthowill1608@gmail.com>
2023-07-10 13:01:13 -0500

    Consulta de categorias

--- backend/servidor.js
+++ backend/servidor.js
@@ -23,33 +23,32 @@ conexion.connect(function(error){
     }
 });
 
-//Obtener Productos
+// 6. obtener la lista de productos
 app.get('/obtenerProductos',(peticion, respuesta)=>{
     // 6.1 consulta sql
     const sql="SELECT * FROM productos";
     // 6.2 lo envio a la conexion
-    conexion.query(sql,(error, resultado)=>{
+    conexion.query(sql,(error,resultado)=>{
         // 6.3 compruebo el resultado
         if(error) return respuesta.json({Error:"Error en la consulta"});
-        return respuesta.json({mensaje:"Exitoso", Resutado:resultado});
+        return respuesta.json({Estatus:"Exitoso",Resultado:resultado});
     });
 });
 
+// 6. obtener categorias
+app.get('/obtenerCategorias',(peticion, respuesta)=>{
+    // 6.1 consulta sql
+    const sql="SELECT * FROM categoria";
+    // 6.2 lo envio a la conexion
+    conexion.query(sql,(error,resultado)=>{
+        // 6.3 compruebo el resultado
+        if(error) return respuesta.json({Error:"Error en la consulta"});
+        return respuesta.json({Estatus:"Exitoso",Resultado:resultado});
+    });
+});
 
 
 //INICIAR SERVIDOR
 app.listen(8082,() =>{
     console.log("Servidor iniciado");
 })
-/*
-app.get('/productos/:id_producto', (peticion, respuesta) =>{
-    const id = peticion.params.id;
-    const sql = "SELECT * FROM productos WHERE id_producto=?";
-    conexion.query(sql, [id], (error, resultado) =>{
-        if (error){
-            return respuesta.json([{Error: "Error en la consulta"}]);
-        }
-        return respuesta.json({mensaje: "Exitoso", Resultado:resultado});
-    });
-});
-*/

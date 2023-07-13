483d1a157e01493bd589a2409841443bd8dd04b8
WilliansAS <anthowill1608@gmail.com>
2023-07-12 22:03:31 -0500

    Resultado final

--- backend/servidor.js
+++ backend/servidor.js
@@ -35,7 +35,7 @@ app.get('/obtenerProductos',(peticion, respuesta)=>{
     });
 });
 
-// 6. obtener categorias
+// 6. Ruta para obtener todas las categorias
 app.get('/obtenerCategorias',(peticion, respuesta)=>{
     // 6.1 consulta sql
     const sql="SELECT * FROM categoria";
@@ -46,6 +46,20 @@ app.get('/obtenerCategorias',(peticion, respuesta)=>{
         return respuesta.json({Estatus:"Exitoso",Resultado:resultado});
     });
 });
+
+//Ruta para obtener los productos por categoria 
+app.get('/obtenerProductos/:id',(peticion, respuesta)=>{
+    const id = peticion.params.id;
+    // 6.1 consulta sql
+    const sql="SELECT * FROM productos WHERE id_categoria_id = ?";
+    // 6.2 lo envio a la conexion
+    conexion.query(sql,(error,resultado)=>{
+        // 6.3 compruebo el resultado
+        if(error) return respuesta.json({Error:"Error en la consulta"});
+        return respuesta.json({Estatus:"Exitoso",Resultado:resultado});
+    });
+});
+
 
 
 //INICIAR SERVIDOR
